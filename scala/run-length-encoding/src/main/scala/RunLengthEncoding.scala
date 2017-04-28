6af19d3c465abf78aef5f08dee19881803068c8e
zombiefungus <divmermarlav@gmail.com>
2017-04-28 09:53:54 -0500

    use patter matching in run-length-encoding solution at scala track to improve readability

--- scala/run-length-encoding/src/main/scala/RunLengthEncoding.scala
+++ scala/run-length-encoding/src/main/scala/RunLengthEncoding.scala
@@ -2,31 +2,31 @@ object RunLengthEncoding {
 
   def encode(input: String): String = {
     input.foldLeft(("", ' ', 0, 1)) {
-      (big_acc, c) => {
-        //big_acc = (acc, last, last_n, pos)
-        if (c == big_acc._2) {
-          if (big_acc._4 == input.length()) { // end of string
-            ( big_acc._1 + (big_acc._3 + 1) + c,
-              big_acc._2,
-              big_acc._3 + 1,
-              big_acc._4 + 1 )
+      case ((acc, last, last_n, pos), c) => {
+        //big_acc =
+        if (c == last) {
+          if (pos == input.length()) { // end of string
+            ( acc + (last_n + 1) + c,
+              last,
+              last_n + 1,
+              pos + 1 )
           }
           else {
-            ( big_acc._1, big_acc._2, big_acc._3 + 1, big_acc._4 + 1 )
+            ( acc, last, last_n + 1, pos + 1 )
           }
         }
         else {
-          var acc = big_acc._1
-          if (big_acc._3 > 1) {
-            acc += big_acc._3;
+          var acc_t = acc
+          if (last_n > 1) {
+            acc_t += last_n;
           }
-          if (big_acc._3 > 0) { // ignore initial last (single whitespace)
-            acc += big_acc._2;
+          if (last_n > 0) { // ignore initial last (single whitespace)
+            acc_t += last;
           }
-          if (big_acc._4 == input.length()) { // end of string
-            acc += c;
+          if (pos == input.length()) { // end of string
+            acc_t += c;
           }
-          (acc, c, 1, big_acc._4 + 1)
+          (acc_t, c, 1, pos + 1)
         }
       }
     }._1
@@ -34,12 +34,12 @@ object RunLengthEncoding {
 
   def decode(input: String): String = {
     input
-      .foldLeft(("", 0)) { (big_acc, c) => {
-        if (Character.isDigit(c)) { // keep reading.. keeeep reading
-          (big_acc._1, 10*big_acc._2 + ("" + c).toInt)
+      .foldLeft(("", 0)) { case ((acc, last ), c) => {
+        if (Character.isDigit(c)) {
+          (acc, 10*last + ("" + c).toInt)
         }
         else {
-          (big_acc._1 + (""+c)*math.max(big_acc._2, 1), 0)
+          (acc + (""+c)*math.max(last, 1), 0)
         }
       }
     }._1
