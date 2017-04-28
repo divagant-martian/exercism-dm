6bc7068af726cc678e3404d1d6c179d8a6e3398d
zombiefungus <divmermarlav@gmail.com>
2017-04-28 09:41:37 -0500

    remove temp variable from word-count solution at track rust

--- rust/word-count/src/lib.rs
+++ rust/word-count/src/lib.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 pub fn word_count(s:&str) ->  HashMap<String, u32> {
-    let ss:String = s.to_lowercase().chars()
+    s.to_lowercase().chars()
         .filter_map(|x|
                     if x.is_alphabetic() || x.is_whitespace() || x.is_numeric() {
                         Some(x)
@@ -8,10 +8,10 @@ pub fn word_count(s:&str) ->  HashMap<String, u32> {
                     else {
                         None
                     })
-        .collect();
-    ss.split_whitespace()
+        .collect::<String>()
+        .split_whitespace()
         .fold(HashMap::new(), |mut acc, w|{
-            *acc.entry(w.to_string()).or_insert(0) += 1;           
+            *acc.entry(w.to_string()).or_insert(0) += 1;
             acc
         })
 }
